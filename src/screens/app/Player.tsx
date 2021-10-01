daf71b7cd35b11f75deb76f09e62a35c8cfa2df1
Thais <thais.marpi@gmail.com>
2021-10-01 12:22:38 -0300

    feat: adiciona imagem com animação

--- src/screens/app/Player.tsx
+++ src/screens/app/Player.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Animated, Easing, StyleSheet, View } from 'react-native'
 import BotaoVoltar from '../../components/BotaoVoltar'
 import Container from '../../components/Container'
 import Titulo from '../../components/Titulo'
 import { IMeditacao } from '../../entities/Meditacao'
 import { PlayerNavigationProps } from '../../routes/App.routes'
 import GetMeditacaoById from '../../services/meditacoes/GetMeditacaoById'
+import ImagemSol from '../../../assets/icon.png'
 import Loading from '../../components/Loading'
 import AudioPlayer from '../../components/AudioPlayer'
 
@@ -14,6 +15,37 @@ const Player = ({ route }: PlayerNavigationProps) => {
 
   const [meditacao, setMeditacao] = useState<IMeditacao>(null)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [rotationOffset, setRotationOffset] = useState(0)
+  let driverRotacao = new Animated.Value(0)
+  const grausRotacao = driverRotacao.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
+
+  const iniciarRotacao = () => {
+    driverRotacao.setOffset(rotationOffset)
+    Animated.loop(
+      Animated.timing(driverRotacao, {
+        toValue: 1,
+        duration: 16000,
+        useNativeDriver: false,
+        easing: Easing.linear
+      })
+    ).start()
+  }
+
+  const pausarRotacao = () => {
+    driverRotacao.stopAnimation(e => {
+      setRotationOffset(e)
+    })
+  }
+
+  useEffect(() => {
+    if (isPlaying) {
+      iniciarRotacao()
+    }
+  }, [isPlaying])
+
   useEffect(() => {
     const buscarMeditacao = async () => {
       const novaMeditacao = await new GetMeditacaoById().call(id)
@@ -24,6 +56,9 @@ const Player = ({ route }: PlayerNavigationProps) => {
   }, [])
 
   const handlePlayPause = (novoEstado: boolean) => {
+    if (isPlaying) {
+      pausarRotacao()
+    }
     setIsPlaying(novoEstado)
   }
 
@@ -35,6 +70,15 @@ const Player = ({ route }: PlayerNavigationProps) => {
           <View>
             <View style={styles.titulo}>
               <Titulo centralizado>{meditacao?.nome}</Titulo>
+            </View>
+            <View style={styles.containerImagem}>
+              <Animated.Image
+                source={ImagemSol}
+                style={[
+                  styles.imagem,
+                  { transform: [{ rotate: grausRotacao }] }
+                ]}
+              />
             </View>
           </View>
           <AudioPlayer source={meditacao?.url} onPlayChange={handlePlayPause} />
@@ -56,5 +100,14 @@ const styles = StyleSheet.create({
   titulo: {
     alignSelf: 'center',
     marginBottom: 16
+  },
+  containerImagem: {
+    maxHeight: 200
+  },
+  imagem: {
+    width: '50%',
+    maxHeight: 200,
+    resizeMode: 'contain',
+    alignSelf: 'center'
   }
 })
